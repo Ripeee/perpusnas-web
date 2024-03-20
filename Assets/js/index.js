cb8c501ef5bfda6a49ae096d385a1ccca39dd78b
Ripeee <arythegodhand@gmail.com>
2024-03-21 05:53:28 +0700

    Updated Navbar Mobile

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -18,6 +18,45 @@ function HamburgerGrup() {
 	document.querySelector(".group-list").classList.toggle("hidden");
 }
 
+function ResensiDropdown() {
+	document.querySelector(".dropdown-icon").classList.toggle("rotate-180");
+	document.querySelector(".list-dropdown").classList.toggle("hidden");
+}
+
+document.querySelector(".beranda").addEventListener("click", () => {
+	document.querySelector(".beranda").classList.add("bg-green-600");
+ document.querySelector(".beranda").classList.add("text-white");
+	document.querySelector(".beranda").classList.remove("text-gray-600");
+	document.querySelector(".resensi").classList.add("text-gray-600");
+	document.querySelector(".resensi").classList.remove("text-white");
+ document.querySelector(".resensi").classList.remove("bg-green-600");
+	document.querySelector(".dropdown-icon").classList.add("rotate-0");
+	document.querySelector(".informasi").classList.add("text-gray-600");
+	document.querySelector(".informasi").classList.remove("hover:text-white");
+	document.querySelector(".informasi").classList.remove("bg-green-600");
+	document.querySelector(".list-dropdown").classList.add("hidden");
+	document.querySelector(".list-dropdown2").classList.add("hidden");
+});
+document.querySelector(".resensi").addEventListener("click", () => {
+ document.querySelector(".beranda").classList.remove("bg-green-600");
+	document.querySelector(".beranda").classList.remove("text-white");
+	document.querySelector(".beranda").classList.add("text-gray-600");
+	document.querySelector(".resensi").classList.add("bg-green-600");
+	document.querySelector(".resensi").classList.remove("text-gray-600");
+	document.querySelector(".resensi").classList.add("text-white");
+	document.querySelector(".resensi").classList.add("bg-green-600");
+	document.querySelector(".resensi").classList.add("text-white");
+});
+document.querySelector(".beranda").addEventListener("click", () => {
+	document.querySelector(".beranda").classList.add("bg-green-600");
+});
+document.querySelector(".beranda").addEventListener("click", () => {
+	document.querySelector(".beranda").classList.add("bg-green-600");
+});
+document.querySelector(".beranda").addEventListener("click", () => {
+	document.querySelector(".beranda").classList.add("bg-green-600");
+});
+
 function Question1() {
 	document.querySelector(".answer-1").classList.toggle("hidden");
 	document.querySelector(".plus").classList.toggle("hidden");
@@ -36,4 +75,3 @@ function Question3() {
 
 document.getElementById("copyright").innerHTML =
 	"&copy Copyright " + new Date().getFullYear() + " | ";
- 
