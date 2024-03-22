203b4bffe4ce84114c1770929b877ef1f3d26d8c
Ripeee <arythegodhand@gmail.com>
2024-03-23 00:05:28 +0700

    Creating Page Form When User Login

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -10,6 +10,178 @@ var swiper = new Swiper(".mySwiper", {
 	},
 });
 
+ CKEDITOR.ClassicEditor.create(document.getElementById("isiBuku"), {
+  toolbar: {
+   items: [
+    'findAndReplace', 'selectAll', '|',
+    'heading', '|',
+    'bold', 'italic', 'strikethrough', 'underline', 'code', 'subscript', 'superscript', 'removeFormat', '|',
+    'bulletedList', 'numberedList', 'todoList', '|',
+    'undo', 'redo',
+    '-',
+    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'highlight', '|',
+    'alignment', '|',
+    'link', 'uploadImage', 'blockQuote', 'insertTable', 'mediaEmbed', '|',
+    'specialCharacters', 'horizontalLine', 'pageBreak', '|',
+    'sourceEditing'
+   ],
+   shouldNotGroupWhenFull: true
+  },
+  list: {
+   properties: {
+    styles: true,
+    startIndex: true,
+    reversed: true
+   }
+  },
+  heading: {
+   options: [
+    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+   ]
+  },
+  placeholder: 'Start typing here...',
+  fontFamily: {
+   options: [
+    'default',
+    'Arial, Helvetica, sans-serif',
+    'Courier New, Courier, monospace',
+    'Georgia, serif',
+    'Lucida Sans Unicode, Lucida Grande, sans-serif',
+    'Tahoma, Geneva, sans-serif',
+    'Times New Roman, Times, serif',
+    'Trebuchet MS, Helvetica, sans-serif',
+    'Verdana, Geneva, sans-serif'
+   ],
+   supportAllValues: true
+  },
+  fontSize: {
+   options: [10, 12, 14, 'default', 18, 20, 22],
+   supportAllValues: true
+  },
+  htmlSupport: {
+   allow: [
+    {
+     name: /.*/,
+     attributes: true,
+     classes: true,
+     styles: true
+    }
+   ]
+  },
+  htmlEmbed: {
+   showPreviews: true
+  },
+  link: {
+   decorators: {
+    addTargetToExternalLinks: true,
+    defaultProtocol: 'https://',
+    toggleDownloadable: {
+     mode: 'manual',
+     label: 'Downloadable',
+     attributes: {
+      download: 'file'
+     }
+    }
+   }
+  },
+  mention: {
+   feeds: [
+    {
+     marker: '@',
+     feed: [
+      '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes', '@chocolate', '@cookie', '@cotton', '@cream',
+      '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread', '@gummi', '@ice', '@jelly-o',
+      '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding', '@sesame', '@snaps', '@soufflé',
+      '@sugar', '@sweet', '@topping', '@wafer'
+     ],
+     minimumCharacters: 1
+    }
+   ]
+  },
+  removePlugins: [
+   'AIAssistant',
+   'CKBox',
+   'CKFinder',
+   'EasyImage',
+   'RealTimeCollaborativeComments',
+   'RealTimeCollaborativeTrackChanges',
+   'RealTimeCollaborativeRevisionHistory',
+   'PresenceList',
+   'Comments',
+   'TrackChanges',
+   'TrackChangesData',
+   'RevisionHistory',
+   'Pagination',
+   'WProofreader',
+   'MathType',
+   'SlashCommand',
+   'Template',
+   'DocumentOutline',
+   'FormatPainter',
+   'TableOfContents',
+   'PasteFromOfficeEnhanced',
+   'CaseChange'
+  ]
+	});
+	
+const dropArea = document.getElementById("dropArea");
+const fileInput = document.getElementById("fileInput");
+const preview = document.getElementById("preview");
+const fileNamePreview = document.getElementById("fileNamePreview");
+
+dropArea.addEventListener("dragover", (e) => {
+	e.preventDefault();
+	dropArea.classList.add("highlight");
+});
+
+dropArea.addEventListener("dragleave", () => {
+	dropArea.classList.remove("highlight");
+});
+
+dropArea.addEventListener("drop", (e) => {
+	e.preventDefault();
+	dropArea.classList.remove("highlight");
+	const file = e.dataTransfer.files[0];
+	updatePreview(file);
+});
+
+fileInput.addEventListener("change", () => {
+	const file = fileInput.files[0];
+	updatePreview(file);
+});
+
+function updatePreview(file) {
+	const reader = new FileReader();
+	reader.onloadend = () => {
+		preview.src = reader.result;
+		preview.classList.remove("hidden");
+		if (file) {
+			fileNamePreview.textContent = file.name;
+			fileNamePreview.classList.remove("hidden");
+			document.querySelector('.choose-file').classList.add('hidden')
+		} else {
+			fileNamePreview.textContent = "";
+			fileNamePreview.classList.add("hidden");
+			document.querySelector(".choose-file").classList.remove("hidden");
+		}
+	};
+	if (file) {
+		reader.readAsDataURL(file);
+	} else {
+		preview.src = "";
+		preview.classList.add("hidden");
+		fileNamePreview.textContent = "";
+		fileNamePreview.classList.add("hidden");
+			document.querySelector(".choose-file").classList.remove("hidden");
+	}
+}
+
 function HamburgerGrup() {
 	document.querySelector(".hamburger").classList.toggle("hidden");
 	document.querySelector(".hamburger").classList.toggle("opcity-0");
@@ -91,6 +263,10 @@ function SearchButton() {
 function CloseSearch() {
 	document.querySelector(".search-bg").classList.toggle("hidden");
 	document.body.classList.toggle("overflow-hidden");
+}
+
+function ProfilButton() {
+	document.querySelector('.dropdown-user').classList.toggle('opacity-0')
 }
 
 document.querySelector(".input").addEventListener("keydown", (event) => {
